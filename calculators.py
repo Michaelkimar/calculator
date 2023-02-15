32bd7c3b7b9161a7a61a8e29ae6f713a978e64ad
michaelkimaru <mgkimaru@gmail.com>
2023-02-15 11:37:10 +0300

    added calculator functions

--- calculators.py
+++ calculators.py
@@ -1,21 +1,35 @@
 # this is a calculator project
-num1=eval(input("Enter number "))
-num2=eval(input("Enter number "))
+number1=eval(input("Enter number "))
+number2=eval(input("Enter number "))
 operator=input("Enter operator ")
 # for addition
 def add(num1,num2):
     result=num1+num2
-    return result
+    print (result)
 
 # for subtraction
 def subtract(num1,num2):
     result=num1-num2
-    return result
+    print (result)
+
 # for division     
 def divide(num1,num2):
     result=num1/num2
-    return result
+    print (result)
+
 #for multiplication
 def multiply(num1,num2):
     result=num1*num2
-    return result    
+    print (result)
+
+if operator =="+":
+    add (number1,number2)
+elif operator=="-":
+        subtract(number1,number2)
+elif operator=="/":
+    divide(number1,number2)
+elif operator=="*" or operator=="x" or operator=="X":
+    multiply(number1,number2)    
+
+else:
+    print("invalid ")                
